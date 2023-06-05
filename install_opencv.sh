f8c643a3e715adb56e4cd6881379c095b96f03dc
Oliver0804 <icetzsr@gmail.com>
2023-06-05 12:48:53 +0800

    第一版尚未測試

---
+++ install_opencv.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+set -e
+
+
+# 檢查 /sbin/dphys-swapfile 中 CONF_MAXSWAP=2048 是否存在
+if grep -q "CONF_MAXSWAP=2048" /etc/dphys-swapfile; then
+    echo "發現系統設置的swap大小為2048MB，您是否希望將其改為4096MB? (y/n)"
+    read response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        sudo sed -i 's/CONF_MAXSWAP=2048/CONF_MAXSWAP=4096/g' /etc/dphys-swapfile
+        echo "swap大小已改為4096MB。請重啟您的機器以應用此更改。"
+        exit 0
+    fi
+else
+    if grep -q "CONF_MAXSWAP=4096" /etc/dphys-swapfile; then
+        echo "swap大小已設定為4096MB，將繼續進行安裝程序"
+    fi
+fi
+
+echo "正在您的Raspberry Pi 32位元作業系統中安裝OpenCV 4.7.0"
+echo "這將至少需要2.5小時！"
+cd ~
+
+# 安裝依賴項
+sudo apt-get install -y build-essential cmake git unzip pkg-config
+sudo apt-get install -y libjpeg-dev libtiff-dev libpng-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev
+sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-gtk3
+sudo apt-get install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-gl
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y python3-dev python3-numpy python3-pip
+sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
+sudo apt-get install -y libv4l-dev v4l-utils
+sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
+sudo apt-get install -y liblapack-dev gfortran libhdf5-dev
+sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y protobuf-compiler
+
+# 下載4.7.0
+cd ~ 
+sudo rm -rf opencv*
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.7.0.zip 
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.7.0.zip 
+# 解壓縮
+unzip opencv.zip 
+unzip opencv_contrib.zip 
+# 為了稍後方便，進行一些管理
+mv opencv-4.7.0 opencv
+mv opencv_contrib-4.7.0 opencv_contrib
+# 清除zip檔案
+rm opencv.zip
+rm opencv_contrib.zip
+
+# 設定安裝目錄
+cd ~/opencv
+mkdir build
+cd build
+
+# 執行cmake
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+-D ENABLE_NEON=ON \
+-D WITH_OPENMP=ON \
+-D WITH_OPENCL=OFF \
+-D BUILD_TIFF=ON \
+-D WITH_FFMPEG=ON \
+-D WITH_TBB=ON \
+-D BUILD_TBB=ON \
+-D WITH_GSTREAMER=ON \
+-D BUILD_TESTS=OFF \
+-D WITH_EIGEN=OFF \
+-D WITH_V4L=ON \
+-D WITH_LIBV4L=ON \
+-D WITH_VTK=OFF \
+-D WITH_QT=ON \
+-D OPENCV_ENABLE_NONFREE=ON \
+-D INSTALL_C_EXAMPLES=OFF \
+-D INSTALL_PYTHON_EXAMPLES=OFF \
+-D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D BUILD_EXAMPLES=OFF ..
+
+# 執行make 樹梅派4B有4核心，所以使用-j4
+make -j4
+sudo make install
+sudo ldconfig
+
+# 清理
+make clean
+sudo apt-get update
+
+echo "完成"
---
+++ readme.md
@@ -0,0 +1,41 @@
+# OpenCV-Pi-Installer
+
+OpenCV-Pi-Installer 是一個專門為 Raspberry Pi 32位元作業系統設計的 OpenCV 4.7.0 安裝腳本。
+
+## 特性
+
+- 安裝 OpenCV 4.7.0。
+- 自動安裝所有依賴項。
+- 檢查並自動配置適當的 swap 大小。
+
+## 使用方法
+
+1. 克隆此 GitHub 專案：
+
+```bash
+git clone https://github.com/<您的帳號>/OpenCV-Pi-Installer.git
+```
+2. 給予腳本執行權限：
+```bash
+cd OpenCV-Pi-Installer
+chmod +x install_opencv.sh
+
+```
+3. 執行腳本：
+```bash
+./install_opencv.sh
+```
+
+## 注意事項
+此腳本在安裝過程中需要進行一些更改，例如調整 swap 大小。請在使用前確保您瞭解這些更改。
+
+請注意，此腳本需要超級使用者權限（sudo）來運行，因為它涉及到軟體的安裝以及系統設定的更改。
+
+## 貢獻
+我們非常歡迎貢獻！請隨時提交 pull requests 來改進這個專案。
+
+
+希望這對您有所幫助！請隨時讓我知道如果您有任何其他問題或需求。
+
+
+
